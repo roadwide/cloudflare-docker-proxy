0937283a8049b6a2ae7819d766ff37d696f4d5dd
roadwide <roadwide@foxmail.com>
2024-06-07 23:04:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.yinyuwei.com": "https://registry-1.docker.io",
+  "quay.yinyuwei.com": "https://quay.io",
+  "gcr.yinyuwei.com": "https://gcr.io",
+  "k8s-gcr.yinyuwei.com": "https://k8s.gcr.io",
+  "k8s.yinyuwei.com": "https://registry.k8s.io",
+  "ghcr.yinyuwei.com": "https://ghcr.io",
+  "cloudsmith.yinyuwei.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
